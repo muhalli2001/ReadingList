d2135b5eb16e4f6fddb1b0171712bd16685ae7ae
muhammad <muhalli2001@knights.ucf.edu>
2025-02-12 23:46:36 -0500

    Using Bash, reference a .json file in the curl command. You could do it in the command line but you can do more than one with bash!

--- curlcommands.sh
+++ curlcommands.sh
@@ -5,5 +5,7 @@ curl -d '{"title":"","year":1000,"runtime":"-123 mins","genres":["sci-fi","sci-f
 
 echo '{ "foo": 123, "bar": 456 }' | jq '.foo'
 
-#curl -H "Content-Type: application/json" --data @body.json http://localhost:8080
+#curl -H "Content-Type: application/json" --data @text.json http://localhost:8080
+
+curl -H "Content-Type: application/json" --data @text.json localhost:4000/v1/movies
 
