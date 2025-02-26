9a29e6023118dc4325a998acc1f9071e87300266
muhammad <muhalli2001@knights.ucf.edu>
2025-02-25 20:16:55 -0500

    some comments before I switched branches oops.

--- internal/data/books.go
+++ internal/data/books.go
@@ -17,3 +17,11 @@ func ValidateSearch(v *validator.Validator, search *Search){
 // store this in data in case it changes anytime soon, it can easily be editted.
 const BooksearchURL = "https://openlibrary.org/search.json"
 const Fields = "&fields=title,key,author_name"
+
+// our covers api will go here most likely.
+// mostly because it *is* an internal thing. It isn't front facing or pingable. 
+// it's barely an api it doesn't ping an external url
+// all it does is create a json struct that.
+// unless i create another internal file? 
+// dang
+// or i create the function somewhere else and create the covers struct here?
